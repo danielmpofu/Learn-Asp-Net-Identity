b06cd81c33d2ad65ed7906497aa89423ea6b85b0
Daniel T Mpofu <50142620+danielmpofu@users.noreply.github.com>
2023-08-17 04:58:23 +0200

    rr

--- Learn Asp Net Identity/Pages/Account/Login.cshtml.cs
+++ Learn Asp Net Identity/Pages/Account/Login.cshtml.cs
@@ -1,6 +1,7 @@
 using System.Security.Claims;
 using Learn_Asp_Net_Identity.DTO;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -27,8 +28,11 @@ namespace Learn_Asp_Net_Identity.Pages.Account
                 new Claim(ClaimTypes.Name, "admin")
             };
             //create an identity from the claims
-            var identity = new ClaimsIdentity(claims);
-            ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+            //var identity = new ClaimsIdentity(claims);
+            var claimsIdentity = new ClaimsIdentity(
+                claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            
+            ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
             await HttpContext.SignInAsync("MyCookie", claimsPrincipal);
             return Redirect("/Index");
         }
--- Learn Asp Net Identity/Startup.cs
+++ Learn Asp Net Identity/Startup.cs
@@ -45,7 +45,7 @@ namespace Learn_Asp_Net_Identity
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapRazorPages(); });
